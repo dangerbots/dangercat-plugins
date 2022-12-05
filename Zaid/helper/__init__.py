e348c2c517cf7a3b703fdaf202f9f407c6fa9311
Santo-Philip <santop7510@gmail.com>
2022-12-05 23:17:18 +0530

    Update __init__.py

--- Zaid/helper/__init__.py
+++ Zaid/helper/__init__.py
@@ -9,6 +9,6 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("TheUpdatesChannel")
+        await client.join_chat("Danger_Bots")
     except BaseException:
         pass
